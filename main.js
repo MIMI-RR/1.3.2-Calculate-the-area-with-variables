7a029c8f204d937f9d62f05833e3f2af368d874a
MIMI-RR <BLESSEDJAMILAH978@GMAIL.COM>
2025-11-19 20:17:32 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-var height = prompt("Enter the height", 150);
-var width = prompt("Enter the width", 230);
+const height = ("300, 150");
+const width = ("500 * 250");
     
 // Write your code below
 
+const area = height * length;
 
 
 
 // Print out result in the console log statement below 
-console.log();
+console.log("The area of the rectangle is: " + area);
